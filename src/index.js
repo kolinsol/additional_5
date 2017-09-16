bcc55706a3ef6b29fdafa621b22bae553b6c862c
Yauheni Tsiarokhin <hihihiko00@gmail.com>
2017-09-16 17:57:39 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, brackets) {
+  let startBrackets = brackets.reduce((acc, cur) => acc.concat([cur[0]]), []);
+  let endBrackets = brackets.reduce((acc, cur) => acc.concat([cur[1]]), []);
+  let bracketsLookup = brackets.reduce((acc, cur) => {
+    acc[cur[1]] = cur[0]
+    return acc;
+  }, {});
+  let res = [];
+  str = str.split("");
+  for(let i = 0; i < str.length; i++) {
+    if (str[i] === bracketsLookup[str[i]]) {
+      if (res.length === 0 || res[res.length - 1] !== str[i]) res.push(str[i]);
+      else res.pop();
+    } else if (startBrackets.includes(str[i])) res.push(str[i]);
+    else if (res.length === 0) return false;
+    else if (res[res.length - 1] === bracketsLookup[str[i]]) res.pop();
+    else return false;
+  }
+  return (res.length === 0) ? true : false;
 }
